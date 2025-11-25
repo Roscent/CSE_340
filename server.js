a0f7937537b183380162557fe4deb17fb78c05fd
roscent <ofu24001@byui.edu>
2025-11-25 21:15:02 +0100

    week1

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -15,6 +16,18 @@ const static = require("./routes/static")
  *************************/
 app.use(static)
 
+// Index route
+app.get("/", function(req, res){
+    res.render("index", {title: "Home"})
+})
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -22,9 +35,21 @@ app.use(static)
 const port = process.env.PORT
 const host = process.env.HOST
 
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => {res.send("Welcome home!")})
+
+
+/* ******************************************
+ * Server host name and port
+ * ***************************************** */
+const HOST = 'localhost'
+const PORT = 5500
+
 /* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+* Log statement to confirm server operation
+* *********************** */
+app.listen(PORT, () => {
+console.log(`trial app listening on ${HOST}:${PORT}`)
 })
